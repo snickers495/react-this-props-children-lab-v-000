5fcd15e847256b4550155af455192592dad52c30
Lisa Lee <snickers495@gmail.com>
2018-02-05 17:31:06 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class React extends React.Component {
   render(){
     return(
+      <div>
       <h1>You've been invited!</h1>
+      </div>
     )
   }
 }
